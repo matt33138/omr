00f94331551257458e793a39ca7c1e1b13e0d50b
Matt Coffman <mattcoff@gmail.com>
2013-07-10 16:26:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# String Love Guitar Practice App
+
+This is the practice application for
+[*String Love Guitar*](http://stringloveguitar.com)
+
+by [Matt Coffman](http://stringloveguitar.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omr::Application.config.secret_key_base = '0ee9f304eabd6960c09fb17661c78fc9e39f543bdbc17c4d5d3ce48a6a7babb1bbbc1a10093c9b9c0c669ac3c3ab165002a08250f8f80c91e590d48a8d3154c8'
